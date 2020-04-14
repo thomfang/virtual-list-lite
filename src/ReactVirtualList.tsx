88520e1c837193cb45026117b5c50b59dc41f9f8
youzhi.fang <youzhi.fang@shopee.com>
2020-04-14 15:03:22 +0800

    support custom style for list container

--- src/ReactVirtualList.tsx
+++ src/ReactVirtualList.tsx
@@ -2,115 +2,116 @@ import React, { useState, useMemo, useRef } from 'react';
 import { VirtualList } from './VirtualList';
 
 const paddingAttrNames = {
-  vertical: {
-    head: 'paddingTop',
-    tail: 'paddingBottom'
-  },
-  horzental: {
-    head: 'paddingLeft',
-    tail: 'paddingRight'
-  }
+    vertical: {
+        head: 'paddingTop',
+        tail: 'paddingBottom',
+    },
+    horzental: {
+        head: 'paddingLeft',
+        tail: 'paddingRight',
+    },
 };
 
 export type ReactVirtualListProps = {
-  list: any[];
-  itemSize: number;
-  bufferCount: number;
-  remainCount?: number;
-  sizeGetter?: (element: HTMLElement) => number;
-  positionGetter?: (element: HTMLElement) => number;
-  verticalLayout?: boolean;
-  onReachTail?: () => void;
-  renderItem: (item: any, index: number) => React.ReactElement;
+    list: any[];
+    itemSize: number;
+    bufferCount: number;
+    remainCount?: number;
+    sizeGetter?: (element: HTMLElement) => number;
+    positionGetter?: (element: HTMLElement) => number;
+    verticalLayout?: boolean;
+    onReachTail?: () => void;
+    containerStyles?: React.CSSProperties;
+    renderItem: (item: any, index: number) => React.ReactElement;
 };
 
 export const ReactVirtualList: React.FC<ReactVirtualListProps> = ({
-  itemSize,
-  bufferCount,
-  remainCount = 0,
-  onReachTail,
-  list,
-  renderItem,
-  verticalLayout = true
-}) => {
-  const [startIndex, setStartIndex] = useState(0);
-  const [endIndex, setEndIndex] = useState(0);
-  const [padding, setPadding] = useState({ head: 0, tail: 0 });
-  const [virtualList] = useState(
-    () => new VirtualList(itemSize, bufferCount, remainCount, verticalLayout)
-  );
-
-  const containerRef = useRef<HTMLDivElement>();
-  const scrollerRef = useRef<HTMLDivElement>();
-  const paddingAttr = verticalLayout
-    ? paddingAttrNames.vertical
-    : paddingAttrNames.horzental;
-
-  const renderList = useMemo(() => list.slice(startIndex, endIndex), [
+    itemSize,
+    bufferCount,
+    remainCount = 0,
+    onReachTail,
     list,
-    startIndex,
-    endIndex
-  ]);
+    renderItem,
+    verticalLayout = true,
+    containerStyles = {},
+}) => {
+    const [startIndex, setStartIndex] = useState(0);
+    const [endIndex, setEndIndex] = useState(0);
+    const [padding, setPadding] = useState({ head: 0, tail: 0 });
+    const [virtualList] = useState(() => new VirtualList(itemSize, bufferCount, remainCount, verticalLayout));
 
-  const scrollHandler = () => {
-    const container = containerRef.current as HTMLDivElement;
-    const scroller = scrollerRef.current as HTMLDivElement;
-    let scrollPosition!: number;
-    let containerSize!: number;
+    const containerRef = useRef<HTMLDivElement>();
+    const scrollerRef = useRef<HTMLDivElement>();
+    const paddingAttr = verticalLayout ? paddingAttrNames.vertical : paddingAttrNames.horzental;
 
-    if (verticalLayout) {
-      scrollPosition = container.scrollTop;
-      containerSize = container.offsetHeight;
-    } else {
-      scrollPosition = container.scrollLeft;
-      containerSize = container.offsetWidth;
-    }
+    const renderList = useMemo(() => {
+        return list.slice(startIndex, endIndex);
+    }, [
+        list,
+        startIndex,
+        endIndex,
+    ]);
 
-    const result = virtualList.compute(
-      containerSize,
-      scrollPosition,
-      list.length,
-      [].slice.call(scroller.children)
+    const scrollHandler = () => {
+        const container = containerRef.current as HTMLDivElement;
+        const scroller = scrollerRef.current as HTMLDivElement;
+        let scrollPosition!: number;
+        let containerSize!: number;
+
+        if (verticalLayout) {
+            scrollPosition = container.scrollTop;
+            containerSize = container.offsetHeight;
+        } else {
+            scrollPosition = container.scrollLeft;
+            containerSize = container.offsetWidth;
+        }
+
+        const result = virtualList.compute(
+            containerSize,
+            scrollPosition,
+            list.length,
+            [].slice.call(scroller.children)
+        );
+
+        setPadding({
+            head: result.paddingHead,
+            tail: result.paddingTail,
+        });
+
+        if (result.shouldUpdate) {
+            setStartIndex(result.startIndex);
+            setEndIndex(result.endIndex);
+        }
+
+        if (result.reachTail) {
+            onReachTail && onReachTail();
+        }
+    };
+
+    return (
+        <div
+            ref={containerRef as any}
+            onScroll={scrollHandler}
+            style={{
+                width: '100%',
+                height: '100%',
+                position: 'relative',
+                ...containerStyles,
+            }}
+        >
+            <div
+                ref={scrollerRef as any}
+                style={{
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    width: '100%',
+                    [paddingAttr.head]: padding.head + 'px',
+                    [paddingAttr.tail]: padding.tail + 'px',
+                }}
+            >
+                {renderList.map((item, index) => renderItem(item, index))}
+            </div>
+        </div>
     );
-
-    setPadding({
-      head: result.paddingHead,
-      tail: result.paddingTail
-    });
-
-    if (result.shouldUpdate) {
-      setStartIndex(result.startIndex);
-      setEndIndex(result.endIndex);
-    }
-
-    if (result.reachTail) {
-      onReachTail && onReachTail();
-    }
-  };
-
-  return (
-    <div
-      ref={containerRef as any}
-      onScroll={scrollHandler}
-      style={{
-        width: '100%',
-        height: '100%',
-        position: 'relative'
-      }}
-    >
-      <div
-        ref={scrollerRef as any}
-        style={{
-          [paddingAttr.head]: padding.head + 'px',
-          [paddingAttr.tail]: padding.tail + 'px',
-          position: 'absolute',
-          left: 0,
-          top: 0,
-          width: '100%'
-        }}
-      >
-        {renderList.map((item, index) => renderItem(item, index))}
-      </div>
-    </div>
-  );
 };
--- src/VirtualList.ts
+++ src/VirtualList.ts
@@ -1,156 +1,158 @@
+type Getter = (element: HTMLElement) => number;
+
 const sizeGetters = {
-  vertical: (element: HTMLElement) => element.offsetHeight,
-  horizental: (element: HTMLElement) => element.offsetWidth
+    vertical: (element: HTMLElement) => element.offsetHeight,
+    horizental: (element: HTMLElement) => element.offsetWidth
 };
 const positionGetters = {
-  vertical: (element: HTMLElement) => element.offsetTop,
-  horizental: (element: HTMLElement) => element.offsetLeft
+    vertical: (element: HTMLElement) => element.offsetTop,
+    horizental: (element: HTMLElement) => element.offsetLeft
 };
 
 export class VirtualList {
-  private startIndex!: number | null;
-  private endIndex!: number | null;
-  private itemDetails: {
-    index: number;
-    pos: number;
-    size: number;
-  }[] = [];
+    private startIndex!: number | null;
+    private endIndex!: number | null;
+    private itemDetails: {
+        index: number;
+        pos: number;
+        size: number;
+    }[] = [];
 
-  private itemSizeGetter!: (itemElement: HTMLElement) => number;
-  private itemPositionGetter!: (itemElement: HTMLElement) => number;
+    private itemSizeGetter!: Getter;
+    private itemPositionGetter!: Getter;
 
-  constructor(
-    public readonly itemSize: number,
-    public readonly bufferCount: number,
-    public readonly remainItemCountToTriggerReachTailEvent = 0,
-    public readonly verticalLayout = true
-  ) {
-    if (verticalLayout) {
-      this.itemSizeGetter = sizeGetters.vertical;
-      this.itemPositionGetter = positionGetters.vertical;
-    } else {
-      this.itemSizeGetter = sizeGetters.horizental;
-      this.itemPositionGetter = positionGetters.horizental;
-    }
-  }
-
-  updateItem(index: number, element: HTMLElement) {
-    this.itemDetails[index] = {
-      index,
-      pos: this.itemPositionGetter(element),
-      size: this.itemSizeGetter(element)
-    };
-  }
-
-  compute(
-    containerSize: number,
-    scrollPosition: number,
-    totalItemsCount: number,
-    renderedItemElements: HTMLElement[]
-  ) {
-    let startIndex!: number;
-    let endIndex!: number;
-    let paddingTail!: number;
-    let shouldUpdate = false;
-    let reachTail = false;
-
-    if (totalItemsCount === 0) {
-      return {
-        startIndex: 0,
-        endIndex: 0,
-        paddingTail: 0,
-        paddingHead: 0,
-        shouldUpdate: false,
-        reachTail: false
-      };
+    constructor(
+        public readonly itemSize: number,
+        public readonly bufferCount: number,
+        public readonly remainItemCountToTriggerReachTailEvent = 0,
+        public readonly verticalLayout = true
+    ) {
+        if (verticalLayout) {
+            this.itemSizeGetter = sizeGetters.vertical;
+            this.itemPositionGetter = positionGetters.vertical;
+        } else {
+            this.itemSizeGetter = sizeGetters.horizental;
+            this.itemPositionGetter = positionGetters.horizental;
+        }
     }
 
-    if (this.startIndex == null && this.endIndex == null) {
-      return this.computeFirstRender(containerSize, totalItemsCount);
+    updateItem(index: number, element: HTMLElement) {
+        this.itemDetails[index] = {
+            index,
+            pos: this.itemPositionGetter(element),
+            size: this.itemSizeGetter(element)
+        };
     }
 
-    renderedItemElements.forEach((elem, i) => {
-      const idx = (this.startIndex as number) + i;
-      if (this.itemDetails[idx]) {
-        return;
-      }
-      this.updateItem(idx, elem);
-    });
+    compute(
+        containerSize: number,
+        scrollPosition: number,
+        totalItemsCount: number,
+        renderedItemElements: HTMLElement[]
+    ) {
+        let startIndex!: number;
+        let endIndex!: number;
+        let paddingTail!: number;
+        let shouldUpdate = false;
+        let reachTail = false;
 
-    const bottom = scrollPosition + containerSize;
-    const itemLength = this.itemDetails.length;
+        if (totalItemsCount === 0) {
+            this.startIndex = null;
+            this.endIndex = null;
+            return {
+                startIndex: 0,
+                endIndex: 0,
+                paddingTail: 0,
+                paddingHead: 0,
+                shouldUpdate: false,
+                reachTail: false
+            };
+        }
 
-    for (let i = 0; i < itemLength; i++) {
-      const itemDetail = this.itemDetails[i];
-      if (itemDetail.pos <= scrollPosition) {
-        startIndex = i;
-      } else if (itemDetail.pos > bottom && endIndex == null) {
-        endIndex = i;
-        break;
-      }
+        if (this.startIndex == null && this.endIndex == null) {
+            return this.computeFirstRender(containerSize, totalItemsCount);
+        }
+
+        renderedItemElements.forEach((elem, i) => {
+            const idx = (this.startIndex as number) + i;
+            if (this.itemDetails[idx]) {
+                return;
+            }
+            this.updateItem(idx, elem);
+        });
+
+        const bottom = scrollPosition + containerSize;
+        const itemLength = this.itemDetails.length;
+
+        for (let i = 0; i < itemLength; i++) {
+            const itemDetail = this.itemDetails[i];
+            if (itemDetail.pos <= scrollPosition) {
+                startIndex = i;
+            } else if (itemDetail.pos > bottom && endIndex == null) {
+                endIndex = i;
+                break;
+            }
+        }
+
+        if (endIndex == null) {
+            endIndex = itemLength;
+        }
+
+        startIndex = Math.max(0, startIndex - this.bufferCount);
+        endIndex = Math.min(totalItemsCount - 1, endIndex + this.bufferCount);
+        const paddingHead = this.itemDetails[startIndex].pos;
+
+        if (endIndex > itemLength - 1) {
+            paddingTail = (totalItemsCount - endIndex - 1) * this.itemSize;
+        } else {
+            paddingTail = (totalItemsCount - itemLength) * this.itemSize;
+            for (let j = endIndex + 1; j < itemLength; j++) {
+                paddingTail += this.itemDetails[j].size;
+            }
+        }
+
+        if (this.startIndex !== startIndex || this.endIndex !== endIndex) {
+            shouldUpdate = true;
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
+        }
+
+        reachTail = totalItemsCount - (endIndex + 1) <= this.remainItemCountToTriggerReachTailEvent;
+
+        return {
+            startIndex,
+            endIndex,
+            paddingHead,
+            paddingTail,
+            shouldUpdate,
+            reachTail
+        };
     }
 
-    if (endIndex == null) {
-      endIndex = itemLength;
+    private computeFirstRender(containerSize: number, totalCount: number) {
+        const startIndex = 0;
+        const visibleCount = Math.ceil(containerSize / this.itemSize);
+        const endIndex = Math.min(this.bufferCount + visibleCount, totalCount);
+        const renderCount = endIndex - startIndex;
+        const paddingHead = 0;
+        const paddingTail = (totalCount - renderCount) * this.itemSize;
+
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+
+        return {
+            startIndex,
+            endIndex,
+            paddingHead,
+            paddingTail,
+            shouldUpdate: true,
+            reachTail: false
+        };
     }
 
-    startIndex = Math.max(0, startIndex - this.bufferCount);
-    endIndex = Math.min(totalItemsCount - 1, endIndex + this.bufferCount);
-    const paddingHead = this.itemDetails[startIndex].pos;
-
-    if (endIndex > itemLength - 1) {
-      paddingTail = (totalItemsCount - endIndex - 1) * this.itemSize;
-    } else {
-      paddingTail = (totalItemsCount - itemLength) * this.itemSize;
-      for (let j = endIndex + 1; j < itemLength; j++) {
-        paddingTail += this.itemDetails[j].size;
-      }
+    reset() {
+        this.startIndex = null;
+        this.endIndex = null;
+        this.itemDetails = [];
     }
-
-    if (this.startIndex !== startIndex || this.endIndex !== endIndex) {
-      shouldUpdate = true;
-      this.startIndex = startIndex;
-      this.endIndex = endIndex;
-    }
-
-    reachTail =
-      totalItemsCount - (endIndex + 1) <=
-      this.remainItemCountToTriggerReachTailEvent;
-
-    return {
-      startIndex,
-      endIndex,
-      paddingHead,
-      paddingTail,
-      shouldUpdate,
-      reachTail
-    };
-  }
-
-  private computeFirstRender(containerSize: number, totalCount: number) {
-    const startIndex = 0;
-    const visibleCount = Math.ceil(containerSize / this.itemSize);
-    const endIndex = Math.min(this.bufferCount + visibleCount, totalCount);
-    const renderCount = endIndex - startIndex;
-    const paddingHead = 0;
-    const paddingTail = (totalCount - renderCount) * this.itemSize;
-
-    this.startIndex = startIndex;
-    this.endIndex = endIndex;
-
-    return {
-      startIndex,
-      endIndex,
-      paddingHead,
-      paddingTail,
-      shouldUpdate: true,
-      reachTail: false
-    };
-  }
-
-  reset() {
-    this.startIndex = null;
-    this.endIndex = null;
-    this.itemDetails = [];
-  }
 }
